926c0bb327e41c74041bf539d441d4b6ecff038e
Piotr Morgwai Kotarbinski <github@morgwai.pl>
2013-03-10 16:27:37 +0100

    Support multiple stl versions

--- NdkTutorial/src/pl/morgwai/ndktutorial/Native.java
+++ NdkTutorial/src/pl/morgwai/ndktutorial/Native.java
@@ -1,10 +1,22 @@
 package pl.morgwai.ndktutorial;
 
+import android.util.Log;
+
 public class Native {
 
+	public static final String LOG_TAG = Native.class.getSimpleName();
+
 	static {
-		System.loadLibrary("stlport_shared");
-		//System.loadLibrary("gnustl_shared");
+		try {
+			System.loadLibrary("stlport_shared");
+		} catch (UnsatisfiedLinkError e) {
+			Log.i(LOG_TAG, "stlport not found");
+		}
+		try {
+			System.loadLibrary("gnustl_shared");
+		} catch (UnsatisfiedLinkError e) {
+			Log.i(LOG_TAG, "gnustl not found");
+		}
 		System.loadLibrary("cryptopp");
 		System.loadLibrary("crypt_user");
 	}
--- cryptopp/Android.mk
+++ cryptopp/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := cryptopp
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcryptopp.so
+LOCAL_SRC_FILES := $(APP_STL)/$(TARGET_ARCH_ABI)/libcryptopp.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_EXPORT_CPPFLAGS := -fexceptions -frtti
 
